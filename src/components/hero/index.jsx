04848ee0cf49472c10fea7b4596951b8e1359c13
AngelloPaperplane <dev@paperplane.co>
2023-07-10 12:07:06 -0500

    Integrate Hero with video

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -84,17 +84,18 @@ const Hero = ({
       align,
       bg_color,
       data_info,
+      videoUrl
     } = dataHero;
     const logoShow = layout === 'principal';
     return (
       <>
-        {logoShow && (pathname === '/' || pathname === '/about-us') && (
+        {logoShow && videoUrl && videoUrl !== '' && (
           <div
             className={`container ${styles.heroContainer} ${styles.heroContainerBefore} flex j-s a-e bg-cv`}>
             <img
               className={`${styles.imgLogo} `}
-              alt=""
-              src={'/icons/logo-hero-black.png'}
+              alt="Logo ThinkTrue"
+              src={image['super-large']}
             />
           </div>
         )}
@@ -111,7 +112,8 @@ const Hero = ({
           style={{
             backgroundColor: layout !== 'poster' ? 'transparent' : bg_color,
           }}>
-          {(pathname === '/' || pathname === '/about-us') && (
+          {/*(pathname === '/' || pathname === '/about-us') && (*/}
+          {videoUrl && videoUrl !== '' && (
             <div className={styles.videoContainer}>
               <button
                 className={`${styles.activeSound} ${
@@ -129,7 +131,7 @@ const Hero = ({
                 preload=""
                 autoPlay
                 controls={isActiveSound}>
-                <source src="https://paperplane.com.co/dev/assets/homevideomini.mp4" />
+                <source src={videoUrl} />
               </video>
             </div>
           )}
