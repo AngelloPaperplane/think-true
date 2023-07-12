b8c44aa2194e0f70cadf361660a27840f612ed8c
AngelloPaperplane <dev@paperplane.co>
2023-07-12 16:32:00 -0500

    Add title on Numbers analitycs

--- src/components/ourNumbers/index.jsx
+++ src/components/ourNumbers/index.jsx
@@ -6,64 +6,7 @@ import styles from './our-numbers.module.css';
 
 const OurNumbers = ({ data }) => {
   const { title, numbers } = data;
-  /*
-  const numbers = [
-    {
-      number: 310,
-      type: 'K',
-      info: 'PEOPLE HAD VIEWING ACCESS TO THE EXPERIENCE',
-      typeNumber: 'number'
-    },
-    {
-      number: 73,
-      type: '%',
-      info: 'OF IN STORE PURCHASERS CHOSE VISA',
-      typeNumber: 'number'
-    },
-    {
-      number: 14,
-      type: 'K',
-      info: 'PEOPLE WALKED AWAY WITH VISA BRANDED SWAG',
-      typeNumber: 'number'
-    },
-    {
-      number: '2050',
-      number: '2,050',
-      type: '',
-      info: 'PARTICIPANTS EXPERIENCED EXCLUSIVE 3D HOLOGRAM CONTENT',
-      typeNumber: 'text'
-    },
-    {
-      number: '$118,77',
-      type: '',
-      info: 'AVERAGE PRICE PER TRANSACTION IN-STORE',
-      typeNumber: 'text'
-    },
-    {
-      number: '$224',
-      type: 'k',
-      info: 'OF PRODUCT',
-      typeNumber: 'text'
-    },
-    {
-      number: '2,050',
-      type: '',
-      info: 'TRANSACTIONS MADE IN-STORE DURING THE DRAFT EXPERIENCE',
-      typeNumber: 'text'
-    },
-  ];
-*/
-  /*
-  const IncrementNumber = ({ n }) => {
-    const { number } = useSpring({
-      from: { number: 0 },
-      number: n,
-      delay: 200,
-      config: { mass: 1, tension: 20, friction: 10 },
-    });
-    return <animated.span>{number.to((n) => n.toFixed(0))}</animated.span>;
-  };
-  */
+  
   useEffect(() => {
     AOS.init();
   }, []);
@@ -89,8 +32,8 @@ const OurNumbers = ({ data }) => {
               data-aos="fade-up"
               data-aos-duration="900"
               data-aos-delay={i * 200}>
-              {1 < 2 && (
-                <p className={`news ${styles.textBeforeNumber}`}>Nearly</p>
+              {number.title!=='' && (
+                <p className={`news ${styles.textBeforeNumber}`}>{number.title}</p>
               )}
               <p className={`news ${styles.bigNumber}`}>
                 {number.prepend}
