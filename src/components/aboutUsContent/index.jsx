5f7dc5d9aa63e360e45a9d63b97b9f5d0c4b338a
ManuelPaperplane <161769617+ManuelPaperplane@users.noreply.github.com>
2024-08-26 15:54:32 -0500

    Fixes for styles

--- src/components/aboutUsContent/index.jsx
+++ src/components/aboutUsContent/index.jsx
@@ -10,6 +10,9 @@ const AboutUsContent = ({ whatdo, whatdo_new }) => {
     AOS.init();
   }, []);
 
+  console.log('whatdo', whatdo);
+  console.log('whatdo_new', whatdo_new);
+
   return (
     <div className={`container ${styles.aboutContainer}`}>
       <h2
@@ -19,47 +22,50 @@ const AboutUsContent = ({ whatdo, whatdo_new }) => {
         className={` ${styles.titleSection}`}
         dangerouslySetInnerHTML={{ __html: whatdo.title }}
       />
-      <div
-        className={`${styles.contentContainer}`}
-        data-aos="fade-up"
-        data-aos-duration="600"
-        data-aos-delay="0">
-        <div
-          className={`${styles.imgPanel}`}
-          style={{
-            backgroundImage: `${`url(${
-              whatdo.image ? whatdo.image['super-large'] : ''
-            })`}`,
-          }}
-        />
 
+      {whatdo && whatdo.sideText !== '' && (
         <div
-          className={`${styles.textContainer}`}
-          dangerouslySetInnerHTML={{ __html: whatdo.sideText }}
-        />
-      </div>
+          className={`${styles.contentContainer}`}
+          data-aos="fade-up"
+          data-aos-duration="600"
+          data-aos-delay="0">
+          <div
+            className={`${styles.imgPanel}`}
+            style={{
+              backgroundImage: `${`url(${
+                whatdo.image ? whatdo.image['super-large'] : ''
+              })`}`,
+            }}
+          />
 
-      <div
-        className={`${styles.contentContainerLeft}`}
-        data-aos="fade-up"
-        data-aos-duration="600"
-        data-aos-delay="0">
+          <div
+            className={`${styles.textContainer}`}
+            dangerouslySetInnerHTML={{ __html: whatdo.sideText }}
+          />
+        </div>
+      )}
+
+      {whatdo_new && whatdo_new.sideText !== '' && (
         <div
-          className={`${styles.imgPanelLeft}`}
-          style={{
-            backgroundImage: `${`url(${
-              whatdo.image ? whatdo_new.image['super-large'] : ''
-            })`}`,
-          }}
-        />
+          className={`${styles.contentContainerLeft}`}
+          data-aos="fade-up"
+          data-aos-duration="600"
+          data-aos-delay="0">
+          <div
+            className={`${styles.imgPanelLeft}`}
+            style={{
+              backgroundImage: `${`url(${
+                whatdo.image ? whatdo_new.image['super-large'] : ''
+              })`}`,
+            }}
+          />
 
-        <div
-          className={`${styles.textContainerLeft}`}
-          dangerouslySetInnerHTML={{ __html: whatdo_new.sideText }}
-        />
-      </div>
-
-      <p className={`${styles.textDescription}`}></p>
+          <div
+            className={`${styles.textContainerLeft}`}
+            dangerouslySetInnerHTML={{ __html: whatdo_new.sideText }}
+          />
+        </div>
+      )}
     </div>
   );
 };
