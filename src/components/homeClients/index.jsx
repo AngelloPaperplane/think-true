19b954ead7b031d55f12c41142867c5fc4cba026
ManuelPaperplane <161769617+ManuelPaperplane@users.noreply.github.com>
2024-08-29 17:47:44 -0500

    spacing carrusel

--- src/components/homeClients/index.jsx
+++ src/components/homeClients/index.jsx
@@ -37,7 +37,7 @@ const Clients = ({ dataContent }) => {
         <div
           className={`${styles.collaboratorsContainer} flex j-c a-c`}
           style={{
-            width: `${clients.length * 16 > 100 ? clients.length * 16 : 100}vw`,
+            width: `${clients.length * 16 > 100 ? (clients.length * 16) : 100}vw`,
           }}>
           {clients.map((logo, i) => (
             <div
@@ -56,8 +56,8 @@ const Clients = ({ dataContent }) => {
         <div
           className={`${styles.collaboratorsContainer} flex j-c a-c`}
           style={{
-            width: `${clients.length * 16 > 100 ? clients.length * 16 : 100}vw`,
-            left: `${clients.length * 16 > 100 ? clients.length * 16 : 100}vw`,
+            width: `${clients.length * 16 > 100 ? (clients.length * 16) : 100}vw`,
+            left: `${clients.length * 16 > 100 ? (clients.length * 16) : 100}vw`,
           }}>
           {clients.map((logo, i) => (
             <div
